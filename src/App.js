3b4e91ccb729e3a7d3e57bfbb3f20af10c363d0b
ivan <ivannovaa.24@gmail.com>
2022-02-12 23:52:42 +0700

    making navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className='container mx-auto bg-slate-400 flex items-center justify-between py-6'>
+
+          <div className="hamburger md:hidden">
+            <div className='rounded bg-gray-800 w-[35px] h-[35px] mr-3'>
+              <h3 className='text-white text-xl text-center pt-1'>E</h3>
+            </div>
+          </div>
+
+          <div className='flex items-center'>
+            <div className='logo flex items-center mr-6'>
+              <div className='rounded bg-gray-800 w-[35px] h-[35px] mr-3'>
+                <h3 className='text-white text-xl text-center pt-1'>E</h3>
+              </div>
+              <h3 className='text-xl'>Epictus</h3>
+            </div>
+            <nav>
+              <ul className='md:flex items-center gap-4 hidden'>
+                <li><a href="#"></a>UI Design</li>
+                <li><a href="#"></a>Front-End</li>
+                <li><a href="#"></a>Back-End</li>
+                <li><a href="#"></a>Lainnya</li>
+                <li><a href="#"></a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div>
+            <input type="text" className='px-6 py-3 rounded-full md:block hidden bg-gray-800 text-white' placeholder='Search' />
+            <div className='rounded bg-gray-800 w-[35px] h-[35px] md:hidden block'>
+              <h3 className='text-white text-xl text-center pt-1'>E</h3>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </>
   );
 }
 
